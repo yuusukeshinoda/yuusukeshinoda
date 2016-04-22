5823cdd2206de725b8406d25fd59ffd246b7efeb
yusuke shinoda <mofcarro1799@gmail.com>
2016-04-22 12:18:24 +0900

    組み込み関数基礎課題７

--- 組み込み関数課題/built_in_kadai7.php
+++ 組み込み関数課題/built_in_kadai7.php
@@ -1,15 +1,10 @@
 <?php
 
 $strings = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
-echo "$strings<br>";
-$search1 = 'U';
-$replace1 = 'う';
-$search2 = 'I';
-$replace2 = 'い';
+$search = array('I','U');
+$replace = array('い','う');
 
-$result1 = str_replace($search1,$replace1,$strings);
-$result2 = str_replace($search2,$replace2,$result1);
-echo "$result1<br>";
-echo $result2;
+$result = str_replace($search,$replace,$strings);
+echo "$result<br>";
 
 ?>
